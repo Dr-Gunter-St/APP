77140f83e879df1e5ca16cd445a3feaa9102d8b3
Nadhila <nadhilanurdin@gmail.com>
2020-01-21 13:03:01 +0700

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,10 +1,58 @@
 import pandas as pd
 import psycopg2
 import sqlalchemy
-import matplotlib as plt
-
-#%matplotlib inline
-
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists, drop_database
 
+# Postgres username, password, and database name
+POSTGRES_ADDRESS = 'http://127.0.0.1/'
+POSTGRES_PORT = '5432'
+POSTGRES_USERNAME = 'postgres'
+POSTGRES_DBNAME = 'city_stat'
+
+# A long string that contains the necessary Postgres login information
+postgres_str = ('postgresql://{username}:{port}/{dbname}'
+                .format(username=POSTGRES_USERNAME,
+                        port=POSTGRES_PORT,
+                        dbname=POSTGRES_DBNAME))
+
+# Create the connection
+# engine = create_engine('postgresql://user:password@localhost:5433/testdb2')
+
+engine = create_engine('postgresql://postgres: @localhost:5432/city_stat')
+
+# input = int(input("Enter a number: "))
+
+# tags = ["highway", "building", "amenity", "emergency", "historic", "office"]
+
+df = pd.read_sql_query(''' select tags->'highway', sum(st_length(linestring::geography)) from ways where tags?'highway' group by tags->'highway' order by sum desc;''',engine)
+df2 = df.rename(columns={'?column?': 'Highway', 'sum': 'Total Lenght'})
+data = df2.groupby('Highway')
+data.size()
+data_total = (data.sum())/1000
+df2['Lenght (KM)'] = df2['Total Lenght']/1000
+my_plot = data_total.plot(kind='bar',legend=None,title="Highway Length in Krakow", figsize=(15,7))
+my_plot.set_xlabel("Highway Type")
+my_plot.set_ylabel("Length (km)")
+print(df2)
+plt.show()
+
+# if (tags[input-1]=="highway"):
+#     df = pd.read_sql_query(
+#         ''' select tags->'highway', sum(st_length(linestring::geography)) from ways where tags?'highway' group by tags->'highway' order by sum desc;''',
+#         engine)
+#     df2 = df.rename(columns={'?column?': 'Highway', 'sum': 'Total Lenght'})
+#     data = df2.groupby('Highway')
+#     data.size()
+#     data_total = (data.sum())/1000
+#     df2['Lenght (KM)'] = df2['Total Lenght']/1000
+#     my_plot = data_total.plot(kind='bar',legend=None,title="Highway Length in Krakow", figsize=(15,7))
+#     my_plot.set_xlabel("Highway Type")
+#     my_plot.set_ylabel("Length (km)")
+#     print(df2)
+#     plt.show()
+# else:
+#     df = pd.read_sql_query('''select tags->'",tags[input-1],"' sum(st_length(linestring::geography)) from ways where tags?'",tags[input-1],"'group by tags->'",tags[input-1],"'",engine)
+#     df2=df.rename(columns={'?column?':tags[input-1]})
+#     print(df2)
